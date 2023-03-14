8d7b6f7d7a48d3f69036593091d6bbab1ec554c3
John Sterrett <58188177+sterrettJD@users.noreply.github.com>
2023-03-14 13:58:54 -0600

    Add long description type for PyPI CI

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
       install_requires=['aiohttp'],
       packages=find_packages(),
       description="KEGG Parser: A tool for parsing and converting KEGG data into manipulable Python objects.",
+      long_description_content_type='text/markdown',
       author="Kumar Thurimella",
       author_email='lozuponelab.dev@olucdenver.onmicrosoft.com',
       url="https://github.com/lozuponelab/KEGG_parser/",
