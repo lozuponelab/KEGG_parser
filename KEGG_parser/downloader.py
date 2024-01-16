51119d5f0c73007c2299467ded833aac9d26d579
John Sterrett <sterrettJD@protonmail.com>
2024-01-16 11:47:44 -0700

    update get_from_kegg_api to skip async

--- KEGG_parser/downloader.py
+++ KEGG_parser/downloader.py
@@ -72,8 +72,11 @@ def kegg_download_manager_synchronous(list_of_ids, wait=1):
     return [raw_record for raw_records in results for raw_record in raw_records.split('///')[:-1]]
 
 
+def get_from_kegg_api(loop, list_of_ids, parser, try_async=True):
+    if try_async == False:
+        print("KEGG parser will try to download data sequentially.")
+        return [parser(raw_record) for raw_record in kegg_download_manager_synchronous(list_of_ids)]
 
-def get_from_kegg_api(loop, list_of_ids, parser):
     try:
         return [parser(raw_record) for raw_record in loop.run_until_complete(kegg_download_manager(loop, list_of_ids))]
     except ValueError:
@@ -81,6 +84,7 @@ def get_from_kegg_api(loop, list_of_ids, parser):
               "This will be slower.")
         time.sleep(30)
         return [parser(raw_record) for raw_record in kegg_download_manager_synchronous(list_of_ids)]
+
 
 def get_kegg_record_dict(list_of_ids, parser, records_file_loc=None, verbose=False):
     if records_file_loc is None:
